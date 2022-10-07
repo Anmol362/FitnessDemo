a76e7506d7be50731fd23a84330738ddbab140dc
Anmol362 <rahangdaleanmol99@gmail.com>
2022-10-07 18:22:11 +0530

    Demo Fitness

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Hero.css";
+
+function Hero() {
+  return (
+    <div className='Hero'>
+        <div className='left-h'>
+          Left side
+        </div>
+        <div className='right-h'>
+          right side
+        </div>
+    </div>
+  )
+}
+export default Hero
